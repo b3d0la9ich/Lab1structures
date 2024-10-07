d1f477bba4c17d82db0110c0373099c8f74ff844
b3d0la9a <chardoma.net@gmail.com>
2024-10-07 20:09:00 +0700

    modified for array

--- header.h
+++ header.h
@@ -25,3 +25,21 @@ struct Queue{
     void pop();
     void display();
 };
+
+struct Array{
+    Node* head;
+    int size;
+    int capacity; // макс вместимость массива
+
+    Array() : head(nullptr), size(0), capacity(0) {}
+
+    Array(int max) : head(nullptr), size(0), capacity(max) {}
+
+    void add(int index, string value);
+    void addToEnd(string value);
+    string get(int index);
+    void remove(int index);
+    void replace(int index, string value);
+    int length();
+    void display();
+};
