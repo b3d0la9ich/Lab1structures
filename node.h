fe6915da6962241aeaad1a2bb9c35fc2260f8bba
b3d0la9a <chardoma.net@gmail.com>
2024-10-10 21:18:28 +0700

    added node

--- node.h
+++ node.h
@@ -1,16 +1,22 @@
-#pragma once 
+#pragma once
+
 #include <iostream>
-#include <string>
 
-using namespace std;
-
-struct Node{
-    string data;
+struct Node {
+    std::string data;
     Node* next;
     Node* prev;
 
-    Node(const string& value, Node* nextNode = nullptr, Node* prevNode = nullptr)
-        :   data(value), next(nextNode), prev(prevNode){
+    Node(const std::string& value, Node* nextNode = nullptr, Node* prevNode = nullptr)
+        : data(value), next(nextNode), prev(prevNode) {
+    }
+};
 
-        }
+struct Node2 {
+    std::string key; // значение узла
+    Node2* left; // указатели на левые и правые дочерние узлы
+    Node2* right;
+    int height; // высота узла
+    // инициализируем дерево
+    Node2(std::string value) : key(value), left(nullptr), right(nullptr), height(1) {}
 };
