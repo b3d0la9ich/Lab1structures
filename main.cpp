8629ce43493277c0d2c7e228af5eb87f6d61a842
b3d0la9a <chardoma.net@gmail.com>
2024-10-15 13:30:21 +0700

    updated for array

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <sstream>
 #include <fstream>
 
+#include <array.hpp>
 #include <singly linked list.hpp>
 #include <double linked list.hpp>
 
 using namespace std;
 
+Array a;
 singlyList sList;
 doubleList dList;
