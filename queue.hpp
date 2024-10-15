4e30bec5c428b0c4dc2ce502f973d91f978c66b3
b3d0la9a <chardoma.net@gmail.com>
2024-10-15 13:33:57 +0700

    updated to hpp

---
+++ queue.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "singly linked list.hpp"
+
+using namespace std;
+
+struct Queue {
+    singlyList qList; // используем команды односвязного листа
+
+    void push(string _item) { // добавление элементов в хвост
+        qList.addToEnd(_item);
+    }
+
+    void pop() { // удаление элемента из головы
+        qList.delFromHead();
+    }
+
+    void findIndex(int index) { // поиск элемента по индексу
+        qList.findIndex(index);
+    }
+
+    void printQueue() { // вывод всех элементов очереди
+        qList.printList();
+    }
+
+    void loadFromFile(const string& file) { // загрузка данных из файла
+       qList.loadFromFile(file);
+    }
+
+    void saveToFile(const string& file) { // сохранение в файл
+        qList.saveToFile(file);
+    }
+};
