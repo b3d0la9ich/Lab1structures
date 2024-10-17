917735adfd910a8a9d894b968a5289e88f5efd88
b3d0la9a <chardoma.net@gmail.com>
2024-10-17 23:15:39 +0700

    letsgoo

---
+++ node.h
@@ -0,0 +1,78 @@
+#pragma once 
+#include <string>
+
+using namespace std;
+
+struct Node {
+    string data;
+    Node* next;
+
+    Node(const string& value, Node* nextNode = nullptr) : data(value), next(nextNode) {}
+};
+
+struct DoubleNode {
+    string data;
+    DoubleNode* next;
+    DoubleNode* prev;
+
+    DoubleNode(const string& value, DoubleNode* nextNode = nullptr, DoubleNode* prevNode = nullptr) : data(value), next(nextNode), prev(prevNode) {}
+};
+
+struct tree_node {
+    int digit;
+    tree_node* right;
+    tree_node* left;
+
+    tree_node(int dig) : digit(dig), left(nullptr), right(nullptr) {}
+};
+
+struct Queue_node {
+    tree_node* tree;
+    Queue_node* next;
+};
+
+struct queue_tree {
+    Queue_node* front;
+    Queue_node* rear;
+    int count;
+
+    queue_tree() : front(nullptr), rear(nullptr), count(0) {}
+
+    bool is_empty(){
+        return count == 0;
+    }
+
+    void enqueue(tree_node* node){
+        Queue_node* new_node = new Queue_node();
+        new_node->tree = node;
+        new_node->next = nullptr;
+
+        if (rear == nullptr) {
+            front = rear = new_node;
+        }
+        else {
+            rear->next = new_node;
+            rear = new_node;
+        }
+
+        count++;
+    }
+
+    tree_node* dequeue(){
+        if (is_empty()){
+            return nullptr;
+        }
+
+        Queue_node* new_node = front;
+        tree_node* res = front->tree;
+        front = front->next;
+
+        if (front == nullptr) {
+            rear = nullptr;
+        }
+
+        delete new_node;
+        count--;
+        return res;
+    }
+};
